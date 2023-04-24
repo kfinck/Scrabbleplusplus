95598de47867b1fffbdfef7702370a2882f49cd1
LiamjohnVelazquez <78558464+LiamjohnVelazquez@users.noreply.github.com>
2023-04-23 23:03:41 -0400

    Update scrabble_objects.cpp

--- scrabble_objects.cpp
+++ scrabble_objects.cpp
@@ -117,13 +117,35 @@ private:
 public:
     bag() {
         map<char, int> tile_distribution = {
-                {'A', 9}, {'B', 2}, {'C', 2}, {'D', 4}, {'E', 12}, {'F', 2},
-                {'G', 3}, {'H', 2}, {'I', 9}, {'J', 1}, {'K', 1}, {'L', 4},
-                {'M', 2}, {'N', 6}, {'O', 8}, {'P', 2}, {'Q', 1}, {'R', 6},
-                {'S', 4}, {'T', 6}, {'U', 4}, {'V', 2}, {'W', 2}, {'X', 1},
-                {'Y', 2}, {'Z', 1}, {'_', 2}
+                {'A', 9},
+                {'B', 2},
+                {'C', 2},
+                {'D', 4},
+                {'E', 12},
+                {'F', 2},
+                {'G', 3},
+                {'H', 2},
+                {'I', 9},
+                {'J', 1},
+                {'K', 1},
+                {'L', 4},
+                {'M', 2},
+                {'N', 6},
+                {'O', 8},
+                {'P', 2},
+                {'Q', 1},
+                {'R', 6},
+                {'S', 4},
+                {'T', 6},
+                {'U', 4},
+                {'V', 2},
+                {'W', 2},
+                {'X', 1},
+                {'Y', 2},
+                {'Z', 1},
+                {'_', 2}
         };
-        for (auto &tile : tile_distribution) {
+        for (auto &tile: tile_distribution) {
             for (int i = 0; i < tile.second; ++i) {
                 tiles.push_back(tile.first);
             }
@@ -132,10 +154,10 @@ public:
 
         srand(time(0));
         random_shuffle(tiles.begin(), tiles.end());
-
+    }
         char draw_tile() {
             if (tiles.empty()) {
-                throw std::runtime_error("Tile bag is empty.");
+                throw runtime_error("Tile bag is empty.");
             }
             char tile = tiles.back();
             tiles.pop_back();
@@ -156,14 +178,16 @@ class player
 private:
     string name;
     int score;
+    vector <char> rack;
 public:
-    char tiles[7];
-
-
-    void placeTiles()
+    player()
     {
 
     }
+
+
+
+    
 
 };
 int main() {
