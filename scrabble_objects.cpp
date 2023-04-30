f5894f9de7661cc580038968e11406c60777f64a
LiamjohnVelazquez <lvelazqu@stevens.edu>
2023-04-30 17:48:57 -0400

    Began implementing scoring functions, added getWordScore, and map for letter values. Need to finish editing place tile, and some other stuff for it to work

--- scrabble_objects.cpp
+++ scrabble_objects.cpp
@@ -33,6 +33,12 @@ class Board: public Tile
 private:
     int placeValue;
     Tile board[15][15];
+    map<char, int> letter_values = {
+            {'A', 1}, {'B', 3}, {'C', 3}, {'D', 2}, {'E', 1}, {'F', 4}, {'G', 2},
+            {'H', 4}, {'I', 1}, {'J', 8}, {'K', 5}, {'L', 1}, {'M', 3}, {'N', 1},
+            {'O', 1}, {'P', 3}, {'Q', 10},{'R', 1}, {'S', 1}, {'T', 1}, {'U', 1},
+            {'V', 4}, {'W', 4}, {'X', 8}, {'Y', 4}, {'Z', 10}, {'_', 0}
+    };
 public:
     char letter;
     HANDLE hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -101,10 +107,20 @@ public:
         }
 
     }
+    int getWordScore(const string& word, const vector<int>& letter_multipliers, const int& word_multiplier) {
+        int score = 0;
+
+        for (size_t i = 0; i < word.size(); i++) {
+            score += letter_values[word[i]] * letter_multipliers[i];
+        }
+
+        return score * word_multiplier;
+    }
 
     void placeTile(int row, int col, char letter)
     {
         board[row][col].letter = letter;
+
     }
 
 };
